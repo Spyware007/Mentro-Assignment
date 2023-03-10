fe5660f23fe0ff709b2c07a3378f89b425fda58e
Spider9480 <omgawandeofficial9834899149@gmail.com>
2023-03-10 06:25:12 +0530

    build fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Prankur from "./assets/Prankur.png";
 import Keshav from "./assets/Keshav.png";
